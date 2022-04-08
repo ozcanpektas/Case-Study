914b51027102c908f5455fd1d223129f6d4bfc2a
Özcan Pektaş <e223778@metu.edu.tr>
2022-04-09 00:55:09 +0300

    class structure improved, error handling mechanism added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import json
-from urllib.request import urlopen
+from sqlite3 import paramstyle
+import urllib.request
+import urllib.error
+from json.decoder import JSONDecodeError
 
 class Node:
     def __init__(self, name, parent, count, price, childs, cost):
@@ -9,62 +12,76 @@ class Node:
         self.price = price
         self.childs = childs
         self.cost = cost
-def createRootNode(dict):
-    root = Node("", None, 0, 0, [], 0)
-    root.name = dict["name"]
-    root.count = dict["count"]
-    root.childs = []
-    root.parent = None
-    root.price = 0
-    root.cost = 0
-    return root
 
-def construct(parent,childs):
-    for key in childs:
-        print(key["name"])
-        childNode = Node("", None, 0, 0, [], 0)
-        childNode.name = key["name"]
-        childNode.count = key["count"]
-        childNode.parent = parent
-        keys = key.keys()
-        if(list(keys)[2] == "price"):
-            childNode.price = key["price"]
-            childNode.childs = []
-            parent.childs.append(childNode)
-        else:
-            childNode.price = 0
-            childs = list(key.values())[2]
-            construct(childNode, childs)
-            parent.childs.append(childNode)
+    def construct(self, childs):
+        for key in childs:
+            print(key["name"])
+            childNode = Node("", None, 0, 0, [], 0)
+            childNode.name = key["name"]
+            childNode.count = key["count"]
+            childNode.parent = self
+            keys = key.keys()
+            if("price" in keys):
+                childNode.price = key["price"]
+                self.childs.append(childNode)
+            else:
+                childs = list(key.values())[2]
+                childNode.construct(childs)
+                self.childs.append(childNode)
 
-def printStructure(root):
-    for key in root.childs:
+    def printStructure(root):
+        for key in root.childs:
+            print(
+                "Name:", key.name , ","
+                " Count:", key.count, ","
+                " Parent:",key.parent.name, ","
+                " Price:", key.price, ","
+                " Cost:", key.cost
+            )
+            if(key.parent != []):
+                key.printStructure()
+
+    def printRoot(root):
         print(
-            "Name:", key.name , ","
-            " Count:", key.count, ","
-            " Parent:",key.parent.name, ","
-            " Price:", key.price, ","
-            " Cost:", key.cost
+            "Root Node: "
+            "Name:", root.name, ","
+            " Count:", root.count, ","
+            " Cost:", root.cost
         )
-        if(key.parent != []):
-            printStructure(key)
 
-def printRoot(root):
-    print(
-        "Root Node: "
-        "Name:", root.name, ","
-        " Count:", root.count, ","
-        " Cost:", root.cost
-    )
+# Reads input from url
+def readInput():
+    try:
+        url = input("Please enter the url or to exit press 'x': ")
+        if(url == "x"):
+            exit()
+        page = urllib.request.urlopen(url)
+        try:
+            dict = json.load(page)
+            return dict
+        except JSONDecodeError as e:
+            print("The url does not contain a json file.")
+            return
+    except urllib.error.HTTPError as err:
+        if err.code == 404:
+            print("Page not found!")
+            return
+        elif err.code == 403:
+            print("Access denied!")
+            return
+        else:
+            print("Something happened! Error code"), err.code
+            return
 
 def main():
-    url = "https://prod-storyly-media.s3.eu-west-1.amazonaws.com/test-scenarios/sample_3.json"
-    response = urlopen(url)
-    dict = json.load(response)
-    root = createRootNode(dict)
-    childs = list(dict.values())[2]
-    construct(root, childs)
-    printRoot(root)
-    printStructure(root)
+    while(1):
+        input = readInput()
+        if(input == None):
+            continue
+        root = Node(input["name"], None, input["count"], 0, [], 0)
+        childs = list(input.values())[2]
+        root.construct(childs)
+        root.printRoot()
+        root.printStructure()
 if __name__ == "__main__":
     main()
